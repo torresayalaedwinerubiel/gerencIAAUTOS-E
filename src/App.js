ae3676af7886f3726d8e68687fd272ae63b3cc63
torresayalaedwinerubiel <142061844+torresayalaedwinerubiel@users.noreply.github.com>
2024-02-03 14:20:33 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <h1>BIENVENIDO</h1>
+  );
+}
+
+export default App;
